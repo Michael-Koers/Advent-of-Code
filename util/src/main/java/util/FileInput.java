686871c3f9cd24f0fd753831c17df551dc3db448
Michael <michaelkoers95@gmail.com>
2023-12-05 18:18:11 +0100

    Properly working with Java 21 now

--- util/src/main/java/util/FileInput.java
+++ util/src/main/java/util/FileInput.java
@@ -12,7 +12,6 @@ public class FileInput {
     public static String INPUT_TEST = "input-test.txt";
 
     public static List<String> read(String filename, Class clazz) throws URISyntaxException, IOException {
-        return Files.readAllLines(Paths.get(clazz.getResource(filename).toURI()
-        ));
+        return Files.readAllLines(Paths.get(clazz.getClassLoader().getResource(filename).toURI()));
     }
 }
