fdf256801e9bac90d76adaa9aa6d391498a0532e
Michael <michaelkoers95@gmail.com>
2023-12-16 19:00:07 +0100

    Tried caching day 16 some more, but it ain't working

--- 2023/day-16-2023/src/main/java/michael/koers/Main.java
+++ 2023/day-16-2023/src/main/java/michael/koers/Main.java
@@ -15,70 +15,69 @@ public class Main {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        List<String> read = FileInput.read(FileInput.INPUT, Main.class);
+        List<String> read = FileInput.read(FileInput.INPUT_TEST, Main.class);
 
         String[][] input = parseInput(read);
 
         Stopwatch stopwatch = new Stopwatch();
         solvePart1(input);
-//        solvePart2(input);
+        solvePart2(input);
         stopwatch.print();
     }
 
-//    private static void solvePart2(String[][] input) {
-//
-//        long max = 0L;
-//
-//        // START TOP
-//        for (int i = 0; i < input[0].length; i++) {
-//            Light beam = new Light(new Point(i, -1), Direction.DOWN);
-//            System.out.printf("Calculating beam %s%n", beam);
-//            Set<Point> points = continueBeam(beam, input);
-//            cachedVisits.clear();
-//            if (points.size() > max) max = points.size();
-//        }
-//
-//        // START BOTTOM
-//        for (int i = 0; i < input[0].length; i++) {
-//            Light beam = new Light(new Point(i, input.length), Direction.UP);
-//            System.out.printf("Calculating beam %s%n", beam);
-//            Set<Point> points = continueBeam(beam, input);
-//            cachedVisits.clear();
-//            if (points.size() > max) max = points.size();
-//        }
-//        // START LEFT
-//        for (int i = 0; i < input.length; i++) {
-//            Light beam = new Light(new Point(-1, i), Direction.RIGHT);
-//            System.out.printf("Calculating beam %s%n", beam);
-//            Set<Point> points = continueBeam(beam, input);
-//            cachedVisits.clear();
-//            if (points.size() > max) max = points.size();
-//        }
-//        // START RIGHT
-//        for (int i = 0; i < input.length; i++) {
-//            Light beam = new Light(new Point(input[0].length, i), Direction.LEFT);
-//            System.out.printf("Calculating beam %s%n", beam);
-//            Set<Point> points = continueBeam(beam, input);
-//            cachedVisits.clear();
-//            if (points.size() > max) max = points.size();
-//        }
-//
-//        System.out.printf("Checked all paths, most covered tiles: %s%n", max - 1);
-//    }
+    private static void solvePart2(String[][] input) {
+
+        long max = 0L;
+
+        // START TOP
+        for (int i = 0; i < input[0].length; i++) {
+            Light beam = new Light(new Point(i, -1), Direction.DOWN);
+            System.out.printf("Calculating beam %s%n", beam);
+            Set<Point> points = continueBeam(beam, input);
+            cachedVisits.clear();
+            if (points.size() > max) max = points.size();
+        }
+
+        // START BOTTOM
+        for (int i = 0; i < input[0].length; i++) {
+            Light beam = new Light(new Point(i, input.length), Direction.UP);
+            System.out.printf("Calculating beam %s%n", beam);
+            Set<Point> points = continueBeam(beam, input);
+            cachedVisits.clear();
+            if (points.size() > max) max = points.size();
+        }
+        // START LEFT
+        for (int i = 0; i < input.length; i++) {
+            Light beam = new Light(new Point(-1, i), Direction.RIGHT);
+            System.out.printf("Calculating beam %s%n", beam);
+            Set<Point> points = continueBeam(beam, input);
+            cachedVisits.clear();
+            if (points.size() > max) max = points.size();
+        }
+        // START RIGHT
+        for (int i = 0; i < input.length; i++) {
+            Light beam = new Light(new Point(input[0].length, i), Direction.LEFT);
+            System.out.printf("Calculating beam %s%n", beam);
+            Set<Point> points = continueBeam(beam, input);
+            cachedVisits.clear();
+            if (points.size() > max) max = points.size();
+        }
+
+        System.out.printf("Checked all paths, most covered tiles: %s%n", max - 1);
+    }
 
     private static void solvePart1(String[][] field) {
         Light start = new Light(new Point(-1, 0), Direction.RIGHT);
         Set<Point> tiles = continueBeam(start, field);
-        cachedVisits.clear();
         System.out.printf("Beam finished travelling, total tiles covered: %s%n", tiles.size() - 1);
     }
 
     private static Set<Point> continueBeam(Light beam, String[][] field) {
 //        System.out.printf("Beam at position %s, going %s%n", beam.position(), beam.direction());
 
-        if (cachedVisits.contains(beam)) {
-            return new HashSet<>();
-        } else {
+        if(cachedVisits.contains(beam)){
+            return Set.of(beam.position());
+        } else{
             cachedVisits.add(beam);
         }
 
@@ -103,55 +102,42 @@ public class Main {
 
         // Hit nothing
         switch (nextSpace) {
-            case "/" -> {
-                hits.addAll(switch (beam.direction()) {
-                    case LEFT -> continueBeam(new Light(nextPos, Direction.DOWN), field);
-                    case RIGHT -> continueBeam(new Light(nextPos, Direction.UP), field);
-                    case UP -> continueBeam(new Light(nextPos, Direction.RIGHT), field);
-                    case DOWN -> continueBeam(new Light(nextPos, Direction.LEFT), field);
-                    default -> throw new NoSuchElementException("Direction " + beam.direction() + " unknown");
-                });
-                return hits;
-            }
-            case "\\" -> {
-                hits.addAll(switch (beam.direction()) {
-                    case LEFT -> continueBeam(new Light(nextPos, Direction.UP), field);
-                    case RIGHT -> continueBeam(new Light(nextPos, Direction.DOWN), field);
-                    case UP -> continueBeam(new Light(nextPos, Direction.LEFT), field);
-                    case DOWN -> continueBeam(new Light(nextPos, Direction.RIGHT), field);
-                    default -> throw new NoSuchElementException("Direction " + beam.direction() + " unknown");
-                });
-                return hits;
-            }
+            case "/" -> hits.addAll(switch (beam.direction()) {
+                case LEFT -> continueBeam(new Light(nextPos, Direction.DOWN), field);
+                case RIGHT -> continueBeam(new Light(nextPos, Direction.UP), field);
+                case UP -> continueBeam(new Light(nextPos, Direction.RIGHT), field);
+                case DOWN -> continueBeam(new Light(nextPos, Direction.LEFT), field);
+                default -> throw new NoSuchElementException("Direction " + beam.direction() + " unknown");
+            });
+            case "\\" -> hits.addAll(switch (beam.direction()) {
+                case LEFT -> continueBeam(new Light(nextPos, Direction.UP), field);
+                case RIGHT -> continueBeam(new Light(nextPos, Direction.DOWN), field);
+                case UP -> continueBeam(new Light(nextPos, Direction.LEFT), field);
+                case DOWN -> continueBeam(new Light(nextPos, Direction.RIGHT), field);
+                default -> throw new NoSuchElementException("Direction " + beam.direction() + " unknown");
+            });
             case "|" -> {
 //                System.out.printf("Beam %s splitting%n", beam);
                 if (beam.direction().equals(Direction.DOWN) || beam.direction().equals(Direction.UP)) {
                     // nothing special
                     hits.addAll(continueBeam(new Light(nextPos, beam.direction()), field));
-                    return hits;
                 } else if (beam.direction().equals(Direction.LEFT) || beam.direction().equals(Direction.RIGHT)) {
                     // Send out light to the top & bottom
                     hits.addAll(continueBeam(new Light(nextPos, Direction.UP), field));
                     hits.addAll(continueBeam(new Light(nextPos, Direction.DOWN), field));
-                    return hits;
                 }
             }
             case "-" -> {
                 if (beam.direction().equals(Direction.LEFT) || beam.direction().equals(Direction.RIGHT)) {
                     // nothing special
                     hits.addAll(continueBeam(new Light(nextPos, beam.direction()), field));
-                    return hits;
                 } else if (beam.direction().equals(Direction.UP) || beam.direction().equals(Direction.DOWN)) {
                     // Send out light to the left & right
                     hits.addAll(continueBeam(new Light(nextPos, Direction.LEFT), field));
                     hits.addAll(continueBeam(new Light(nextPos, Direction.RIGHT), field));
-                    return hits;
                 }
             }
-            default -> {
-                hits.addAll(continueBeam(new Light(nextPos, beam.direction()), field));
-                return hits;
-            }
+            default -> hits.addAll(continueBeam(new Light(nextPos, beam.direction()), field));
         }
         return hits;
     }
