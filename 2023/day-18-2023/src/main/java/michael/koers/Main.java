ef6100b29020be8f36044da57801cbbcdb2b8731
Michael Koers <michael.koers@infosupport.com>
2023-12-18 08:53:45 +0100

    Tried with Floodfill

--- 2023/day-18-2023/src/main/java/michael/koers/Main.java
+++ 2023/day-18-2023/src/main/java/michael/koers/Main.java
@@ -6,14 +6,12 @@ import util.Point;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        List<String> read = FileInput.read(FileInput.INPUT_TEST, Main.class);
+        List<String> read = FileInput.read(FileInput.INPUT, Main.class);
 
         List<Point> trenches = parseInput(read);
 
@@ -30,18 +28,40 @@ public class Main {
 
     private static void solvePart1(List<Point> trenches, Field field) {
 
-        int n = trenches.size();
-        double a = 0.0;
-        for (int i = 0; i < n - 1; i++) {
-            a += trenches.get(i).x() * trenches.get(i + 1).y() - trenches.get(i + 1).x() * trenches.get(i).y();
-        }
-        a  = Math.abs(a + trenches.get(n - 1).x() * trenches.get(0).y() - trenches.get(0).x() * trenches.get(n - 1).y()) / 2.0;
+        int start_y = trenches.stream().mapToInt(Point::y).min().getAsInt() + 1;
+        int start_x = trenches.stream().filter(p -> p.y() == start_y).mapToInt(Point::x).min().getAsInt() + 1;
+
+        Point start = new Point(start_x, start_y);
+
+        System.out.println("Starting position: " + start);
+
+        Set<Point> inside = floodFill(trenches,new HashSet<>(), start);
+        inside.addAll(trenches);
 
         prettyPrint(trenches, field);
-        System.out.printf("Solved part 1, total cubic meters: %s%n", a);
+        System.out.printf("Solved part 1, total cubic meters: %s%n", inside.size());
     }
 
-    private static void prettyPrint(List<Point> trenches, Field field) {
+    private static Set<Point> floodFill(List<Point> trenches, Set<Point> inside, Point next) {
+
+        if (trenches.contains(next)) {
+            return inside;
+        } else if (inside.contains(next)) {
+            return inside;
+        }
+
+        inside.add(next);
+
+        // Go left, right, up and down
+        inside.addAll(floodFill(trenches, inside, next.moveDirection(Direction.UP)));
+        inside.addAll(floodFill(trenches, inside, next.moveDirection(Direction.RIGHT)));
+        inside.addAll(floodFill(trenches, inside, next.moveDirection(Direction.LEFT)));
+        inside.addAll(floodFill(trenches, inside, next.moveDirection(Direction.DOWN)));
+
+        return inside;
+    }
+
+    private static void prettyPrint(Collection<Point> trenches, Field field) {
         for (int y = field.start_y(); y <= field.end_y(); y++) {
             for (int x = field.start_x(); x <= field.end_x(); x++) {
                 if (trenches.contains(new Point(x, y))) {
@@ -62,7 +82,6 @@ public class Main {
         List<Point> points = new ArrayList<>();
         points.add(current);
 
-        int edge = 0;
         for (String s : read) {
 
             // todo: something with color
@@ -71,7 +90,7 @@ public class Main {
             String direction = parts[0];
             int meters = Integer.parseInt(parts[1]);
 //            int color = parts[2];
-            edge += meters - 2;
+
             for (int i = 0; i < meters; i++) {
                 switch (direction) {
                     case "R" -> current = current.moveDirection(Direction.RIGHT);
@@ -80,10 +99,9 @@ public class Main {
                     case "U" -> current = current.moveDirection(Direction.UP);
                     default -> throw new NoSuchElementException("Direction " + direction + " unknown");
                 }
+                points.add(current);
             }
-            points.add(current);
         }
-        System.out.println(edge);
         return points;
     }
 }
