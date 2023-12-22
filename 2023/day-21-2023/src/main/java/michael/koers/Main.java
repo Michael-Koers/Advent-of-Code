116d72abc74d3e75a8790126e98e7d43718bc905
Michael <michaelkoers95@gmail.com>
2023-12-22 16:03:42 +0100

    Internet helped me solve day 21 part 2

--- 2023/day-21-2023/src/main/java/michael/koers/Main.java
+++ 2023/day-21-2023/src/main/java/michael/koers/Main.java
@@ -17,14 +17,74 @@ public class Main {
 
         Field field = parseInput(read);
 
-        solvePart1(field, 64);
+//        solvePart1(field, 64);
+        solvePart2(field, 26501365L);
+    }
+
+    private static void solvePart2(Field field, long steps) {
+        // We will have even/odd grids that are fully reachable, and some grids at the far reaches that we can't fully cover
+
+        // Field is a perfect square
+        assert field.rows() == field.cols() : "Assertion is false";
+        int grid_size = field.cols();
+
+        // We start in the middle of the grid
+        assert field.start().y() == field.start().x() && field.start().x() == Math.floorDiv(grid_size, 2) : "Assertion is false";
+
+        // Number of steps equals to 1 half grid + full grid sizes, with which we prove that we will perfectly reach
+        // the end of a grid with our final step in straight horizontal / vertical direction
+        assert steps % grid_size == Math.floorDiv(grid_size, 2) : "Assertion is false";
+
+        // Number of grids that we will be able to fully cover with our number of steps
+        long grid_width = Math.floorDiv(steps, grid_size) -1;
+
+        // Number of odd grids (meaning, starting grid with odd number of steps)
+        long odd_grids = (long) Math.pow(Math.floorDiv(grid_width, 2) * 2 + 1, 2);
+        // Number of even grids (meaning, starting grid with even number of steps)
+        long even_grids = (long) Math.pow(Math.floorDiv(grid_width+1, 2) * 2, 2);
+
+        // These are the odds/even grids we will fully cover, so calculate to total number of steps once, starting point doesn't matter
+        // Just make sure that we fully cover the grid with our steps
+        long odd_grid_steps = calculatePlotsReached(field.start(), field, grid_size *2+1);
+        long even_grid_steps = calculatePlotsReached(field.start(), field, grid_size *2);
+
+        // Calculate number of plots reached for outermost grids on top/left/bottom/right
+        long steps_t = calculatePlotsReached(new Point(field.start().x(), grid_size-1), field, grid_size-1);
+        long steps_r = calculatePlotsReached(new Point(0, field.start().y()), field, grid_size-1);
+        long steps_b = calculatePlotsReached(new Point(field.start().x(), 0), field, grid_size-1);
+        long steps_l = calculatePlotsReached(new Point(grid_size-1, field.start().y()), field, grid_size-1);
+
+        // Calculate small corner fields reached
+        long small_tr = calculatePlotsReached(new Point(0 , grid_size-1), field, Math.floorDiv(grid_size, 2) - 1);
+        long small_tl = calculatePlotsReached(new Point(grid_size-1 , grid_size-1), field, Math.floorDiv(grid_size, 2) - 1);
+        long small_br = calculatePlotsReached(new Point(0 , 0), field, Math.floorDiv(grid_size, 2) - 1);
+        long small_bl = calculatePlotsReached(new Point(grid_size-1 , 0), field, Math.floorDiv(grid_size, 2) - 1);
+
+        // Calculate large corner fields reached
+        long large_tr = calculatePlotsReached(new Point(0 , grid_size-1), field, Math.floorDiv(grid_size * 3, 2) - 1);
+        long large_tl = calculatePlotsReached(new Point(grid_size-1 , grid_size-1), field, Math.floorDiv(grid_size * 3, 2) - 1);
+        long large_br = calculatePlotsReached(new Point(0 , 0), field, Math.floorDiv(grid_size * 3, 2) - 1);
+        long large_bl = calculatePlotsReached(new Point(grid_size-1 , 0), field, Math.floorDiv(grid_size * 3, 2) - 1);
+
+        long total =
+                odd_grids * odd_grid_steps                  // All odd grid steps
+                + even_grids * even_grid_steps              // All even grid steps
+                + steps_t + steps_r + steps_b + steps_l     // All straight outermost grids steps
+                + (grid_width + 1) * (small_tr + small_tl + small_br + small_bl) // All small outer grids steps
+                + grid_width * (large_tr + large_tl + large_br + large_bl); // All large outer grids steps
+
+        System.out.printf("Solved part 2, total plots reached : %s%n", total);
     }
 
     private static void solvePart1(Field field, int steps) {
+        long totalSteps = calculatePlotsReached(field.start(), field, steps);
+        System.out.printf("Solved part 1, total garden plots reached: %s%n", totalSteps);
+    }
 
+    private static Long calculatePlotsReached(Point start, Field field, int steps) {
         Set<Point> nextSteps = new HashSet<>();
         Set<Point> currentSteps = new HashSet<>();
-        currentSteps.add(field.start());
+        currentSteps.add(start);
 
         for (int i = 0; i < steps; i++) {
 
@@ -39,18 +99,17 @@ public class Main {
             currentSteps = new HashSet<>(nextSteps);
             nextSteps.clear();
         }
-        prettyPrint(field, currentSteps);
-        System.out.printf("Solved part 1, total garden plots reached: %s%n", currentSteps.size());
+        return (long) currentSteps.size();
     }
 
     private static void prettyPrint(Field field, Set<Point> currentSteps) {
         for (int y = 0; y < field.rows(); y++) {
             for (int x = 0; x < field.cols(); x++) {
-                if(currentSteps.contains(new Point(x, y))){
+                if (currentSteps.contains(new Point(x, y))) {
                     System.out.print("O");
-                } else if (field.rocks().contains(new Point(x, y))){
+                } else if (field.rocks().contains(new Point(x, y))) {
                     System.out.print("#");
-                } else{
+                } else {
                     System.out.print(".");
                 }
             }
