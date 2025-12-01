a05343d161601b64d5287a38a1c10832ae8a82c9
Michael Koers <michael.koers@befrank.nl>
2025-12-01 07:14:51 +0100

    Day 1

---
+++ 2025/src/config/Year2025.java
@@ -0,0 +1,34 @@
+package config;
+
+import util.Stopwatch;
+import util.Year;
+
+import java.util.List;
+
+public abstract class Year2025 implements Year {
+
+    public void solve(List<String> lines) {
+        this.stopwatch.start();
+        this.solvePart1(lines);
+        this.stopwatch.time();
+        this.solvePart2(lines);
+        this.stopwatch.time();
+        this.stopwatch.prettyPrint();
+    }
+
+    public Stopwatch stopwatch;
+
+    public Year2025() {
+        stopwatch = new Stopwatch(this.getPackage());
+    }
+
+    @Override
+    public String getPackage() {
+        return this.getClass().getPackageName();
+    }
+
+    @Override
+    public int getYear() {
+        return 2025;
+    }
+}
---
+++ 2025/src/day01/Day01.java
@@ -0,0 +1,79 @@
+package day01;
+
+import config.Year2025;
+import java.io.IOException;
+import java.util.List;
+
+public class Day01 extends Year2025 {
+
+    public static void main(String[] args) throws IOException {
+        var d = new Day01();
+        var lines = d.readInput();
+
+        d.solve(lines);
+    }
+
+    @Override
+    public void solvePart1(final List<String> lines) {
+
+        int start = 50;
+        int max = 100;
+        int count = 0;
+
+//        System.out.println(start);
+
+        for (final String line : lines) {
+            var amount = Integer.parseInt(line.substring(1));
+            var dir = line.charAt(0);
+
+            if (dir == 'L') {
+                start -= amount;
+            } else if (dir == 'R') {
+                start += amount;
+            }
+            start = ((start % max) + max) % max;
+            if (start == 0) {count++;}
+
+//            System.out.println(start);
+
+        }
+
+        System.out.println(count);
+
+    }
+
+    @Override
+    public void solvePart2(final List<String> lines) {
+
+        int pointer = 50;
+        int max = 100;
+        int count = 0;
+
+
+        for (final String line : lines) {
+            var amount = Integer.parseInt(line.substring(1));
+            var dir = line.charAt(0);
+
+            for(int i = 0; i < amount; i++) {
+
+                if(dir == 'L') {
+                    pointer--;
+                } else if (dir == 'R') {
+                    pointer++;
+                }
+
+                if(pointer < 0) {
+                    pointer = max;
+                }
+                if(pointer > max){
+                    pointer = 0;
+                }
+
+                if(pointer == 0) {count++;}
+            }
+        }
+
+        System.out.println("Clicks: " + count);
+
+    }
+}
